a22552cf9cdacdbc2f468e46d1448b967bca0b2b
cute0laf <oliverlim818@gmail.com>
2023-07-19 17:08:03 -0700

    show substrate address

--- src/components/account/AccountInfoTable.tsx
+++ src/components/account/AccountInfoTable.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { HTMLAttributes } from "react";
-import { isEthereumAddress } from "@polkadot/util-crypto";
+import { encodeAddress, isEthereumAddress } from "@polkadot/util-crypto";
 
 import { Account } from "../../model/account";
 import { Resource } from "../../model/resource";
@@ -26,6 +26,11 @@ export const AccountInfoTable = (props: AccountInfoTableProps) => {
 			error={account.error}
 			{...tableProps}
 		>
+			<AccountInfoTableAttribute
+				label="Substrate address"
+				render={(data) => encodeAddress(data.address, 42)}
+				copyToClipboard={(data) => encodeAddress(data.address, 42)}
+			/>
 			<AccountInfoTableAttribute
 				label={(data) => isEthereumAddress(data.address) ? "Address" : "Public key"}
 				render={(data) => data.address}
