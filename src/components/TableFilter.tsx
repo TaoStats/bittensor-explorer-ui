61d83e0696223f4fa439aa7a7ea28fdfb0606d16
TopETH <supernathanliu@gmail.com>
2023-12-07 14:15:58 -0500

    fix: make table pagination responsive on mobile view

--- src/components/TableFilter.tsx
+++ src/components/TableFilter.tsx
@@ -17,15 +17,23 @@ const filterLabelStyle = (theme: Theme) => css`
   margin-right: 5px;
 `;
 
+const filterValuesStyle = () => css`
+  flex: 1;
+`;
+
 const filterItemStyle = (theme: Theme) => css`
   color: ${theme.palette.secondary.main};
   cursor: pointer;
+  float: left;
+  margin-right: 10px;
 `;
 
 const filterItemSelectedStyle = (theme: Theme) => css`
   color: ${theme.palette.secondary.light};
   cursor: pointer;
   font-weight: bold;
+  float: left;
+  margin-right: 10px;
 `;
 
 type TableFilterProps = {
@@ -42,24 +50,26 @@ export function TableFilter(props: TableFilterProps) {
 	return (
 		<div css={filterStyle}>
 			<label css={filterLabelStyle}>{filter.key}</label>
-			{filter.values.map((breakpoint: any, index: number) => (
-				<div
-					css={breakpoint === value ? filterItemSelectedStyle : filterItemStyle}
-					key={index}
-					onClick={() => {
-						if (onFilterChange) onFilterChange(property, breakpoint);
+			<div css={filterValuesStyle}>
+				{filter.values.map((breakpoint: any, index: number) => (
+					<div
+						css={breakpoint === value ? filterItemSelectedStyle : filterItemStyle}
+						key={index}
+						onClick={() => {
+							if (onFilterChange) onFilterChange(property, breakpoint);
 
-						pagination?.set({
-							...pagination,
-							offset: 1,
-							page: 1,
-							prevEndCursor: [],
-						});
-					}}
-				>
-					{filter.labels[index]}
-				</div>
-			))}
+							pagination?.set({
+								...pagination,
+								offset: 1,
+								page: 1,
+								prevEndCursor: [],
+							});
+						}}
+					>
+						{filter.labels[index]}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
