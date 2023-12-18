474ead115af39376ea8f4179c0d3396db84bb790
cuteolaf <oliverlim818@gmail.com>
2023-12-18 06:55:19 -0800

    link fix

--- src/hooks/useTokenStats.ts
+++ src/hooks/useTokenStats.ts
@@ -6,49 +6,49 @@ import { DataError } from "../utils/error";
 import { TokenStats, TokenStatsResponse } from "../model/tokenStats";
 
 export function useTokenStats(): TokenStatsResponse {
-  const rollbar = useRollbar();
+	const rollbar = useRollbar();
 
-  const [data, setData] = useState<TokenStats[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<DataError>();
+	const [data, setData] = useState<TokenStats[]>([]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<DataError>();
 
-  const fetchData = useCallback(async () => {
-    try {
-      const limit = 100;
+	const fetchData = useCallback(async () => {
+		try {
+			const limit = 100;
 
-      let finished = false;
-      let after: string | undefined = undefined;
+			let finished = false;
+			let after: string | undefined = undefined;
 
-      const result: TokenStats[] = [];
-      while (!finished) {
-        const stats = await getTokenStats(after, limit);
-        result.push(...stats.data);
-        finished = !stats.hasNextPage;
-        after = stats.endCursor;
-      }
-      setData(result);
-    } catch (e) {
-      if (e instanceof DataError) {
-        rollbar.error(e);
-        setError(e);
-      } else {
-        throw e;
-      }
-    }
+			const result: TokenStats[] = [];
+			while (!finished) {
+				const stats = await getTokenStats(after, limit);
+				result.push(...stats.data);
+				finished = !stats.hasNextPage;
+				after = stats.endCursor;
+			}
+			setData(result);
+		} catch (e) {
+			if (e instanceof DataError) {
+				rollbar.error(e);
+				setError(e);
+			} else {
+				throw e;
+			}
+		}
 
-    setLoading(false);
-  }, []);
+		setLoading(false);
+	}, []);
 
-  useEffect(() => {
-    setData([]);
-    setError(undefined);
-    setLoading(true);
-    fetchData();
-  }, [fetchData]);
+	useEffect(() => {
+		setData([]);
+		setError(undefined);
+		setLoading(true);
+		fetchData();
+	}, [fetchData]);
 
-  return {
-    loading,
-    error,
-    data,
-  };
+	return {
+		loading,
+		error,
+		data,
+	};
 }
