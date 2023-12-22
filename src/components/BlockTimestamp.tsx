a49b1e4ffbaab45cd27759d1c952e9fc389a5acc
cuteolaf <oliverlim818@gmail.com>
2023-12-21 19:20:53 -0800

    fix fetch block timestamp

--- src/components/BlockTimestamp.tsx
+++ src/components/BlockTimestamp.tsx
@@ -1,7 +1,17 @@
+/** @jsxImportSource @emotion/react */
 import { Time, TimeProps } from "./Time";
 import Spinner from "../components/Spinner";
-import useSWRImmutable from "swr/immutable";
+import ErrorIcon from "@mui/icons-material/Warning";
+import { css } from "@emotion/react";
 import { fetchBlockTimestamp } from "../utils/block";
+import { useResource } from "../hooks/useResource";
+
+const errorIconStyle = css`
+  margin-left: 8px;
+  position: relative;
+  top: 1px;
+  color: #ef5350;
+`;
 
 interface BlockTimestampProps extends Omit<TimeProps, "time"> {
 	blockHeight: bigint;
@@ -10,15 +20,16 @@ export const BlockTimestamp = ({
 	blockHeight,
 	...props
 }: BlockTimestampProps) => {
-	const { data, isLoading, error } = useSWRImmutable(
-		blockHeight.toString(),
-		fetchBlockTimestamp
-	);
+	const { data, loading, error } = useResource(fetchBlockTimestamp, [
+		blockHeight,
+	]);
 
-	return isLoading ? (
+	return loading ? (
 		<Spinner small />
-	) : error || !data ? (
-		<></>
+	) : error ? (
+		<ErrorIcon css={errorIconStyle} />
+	) : !data ? (
+		<>Nakamoto</>
 	) : (
 		<Time time={data} {...props} />
 	);
